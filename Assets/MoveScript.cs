af695a4a391a2549c91c2d889d1334bf81401794
Валік Мазурок <valentin.mazurok@gmail.com>
2019-04-09 00:57:44 +0300

    tarrain end camera movement

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+	private float speed = 1;
+    void Start()
+    {
+		Cursor.visible = false;
+    }
+
+    void Update()
+    {
+		Vector3 mouseRot = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+		transform.eulerAngles += mouseRot;
+		Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		Vector3 finalMoveVector = Quaternion.Euler(transform.eulerAngles) * moveVector;
+		if (Input.GetKey(KeyCode.LeftShift))
+			speed = 2;
+		else
+			speed = 1;
+		GetComponent<Rigidbody>().MovePosition(transform.position + finalMoveVector * speed);
+		
+	}
+}
